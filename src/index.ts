78e80c6532fd29439e4c8af72aa0e0ff0ed101e5
ycha613 <ycha613@aucklanduni.ac.nz>
2026-02-28 16:08:39 +1300

    added function to get mini league standings

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,46 @@
 // index file
 
+import dotenv from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
+import { getMiniLeagueStandings } from "./services/standingsService";
+
+
+dotenv.config();
+
 console.log("testing");
+
+
+const leagueId = "240333"
+const testApi = `https://fantasy.premierleague.com/api/leagues-classic/${leagueId}/standings/`
+
+async function testApiCall() {
+    const playerStandings = await getMiniLeagueStandings(leagueId);
+    for (const player of playerStandings) {
+        console.log(player);
+    }
+}
+testApiCall();
+
+
+
+
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+client.once("clientReady", () => {
+    console.log("Logged in");
+})
+
+client.on("messageCreate", async (message) => {
+    // ignore messages from other bots
+    if (message.author.bot) return;
+})
+
+client.login(process.env.DISCORD_TOKEN);
+
---
+++ src/services/standingsService.ts
@@ -0,0 +1,46 @@
+// standingsService
+
+export interface PlayerData {
+  playerName: string;
+  playerId: number;
+  playerRank: number;
+  playerRankSort: number;
+  playerLastRank: number;
+  playerPoints: number;
+  playerTeamName: string;
+}
+
+
+export async function getMiniLeagueStandings(leagueId: string): Promise<PlayerData[]> {
+    const apiUrl = `https://fantasy.premierleague.com/api/leagues-classic/${leagueId}/standings/`;
+    try {
+        // call api and check response is ok
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error status: ${response.status}`);
+        }
+
+        // extract player standings data
+        const data = await response.json();
+        //console.log(JSON.stringify(data.standings.results, null, 2));
+        const standings = data.standings.results;
+        const mapped: PlayerData[] = standings.map((player: any) => ({
+            playerName: player.player_name,
+            playerId: player.id,
+            playerRank: player.rank,
+            playerRankSort: player.rank_sort,
+            playerLastRank: player.last_rank,
+            playerPoints: player.total,
+            playerTeamName: player.entry_name,
+        }));
+
+        // sort players by rank_sort and return
+        mapped.sort((a, b) => a.playerRankSort - b.playerRankSort);
+        return mapped;
+
+
+    } catch (error) {
+        console.error("Error fetching data from FPL API:", error);
+        throw error;
+    }
+};
