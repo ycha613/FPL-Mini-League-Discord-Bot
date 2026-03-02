b38b45a1e7ee065e62456f00ff269c2492c7404d
ycha613 <ycha613@aucklanduni.ac.nz>
2026-03-02 13:45:08 +1300

    added command /id name to get player ids for specific name for use in other commands

--- src/commands/id.ts
+++ src/commands/id.ts
@@ -1,4 +1,4 @@
-// src/commands/ids.ts
+// src/commands/id.ts
 
 import { ChatInputCommandInteraction, EmbedBuilder, PermissionsBitField } from "discord.js";
 import Command from "../base/classes/Command";
@@ -7,16 +7,23 @@ import Category from "../base/enums/Category";
 import { getMiniLeaguePlayerDetails } from "../services/playerDetailsService";
 
 
-export default class Ids extends Command {
+export default class Id extends Command {
     constructor(client: CustomClient) {
         super(client, {
-            name: "ids",
-            description: "Displays the player ids of all members of the mini league", 
+            name: "id",
+            description: "Gets the ids of players with a specific player name in the mini league.", 
             category: Category.Utilities,
             default_member_permissions: PermissionsBitField.Flags.UseApplicationCommands,
             dm_permission: true,
             cooldown: 3,
-            options: []
+            options: [
+                {
+                    name: "name",
+                    description: "name of the player (fpl account)",
+                    type: 3, // string
+                    required: true
+                }
+            ]
         });
     }
 
@@ -26,14 +33,30 @@ export default class Ids extends Command {
         const leagueId = this.client.config.leagueId;
 
         try {
+            const name = interaction.options.getString("name", true);
             const playerData = await getMiniLeaguePlayerDetails(leagueId);
-            const top10 = playerData.slice(0, 10); // only first 10
-            const rows = top10.map((player, index) => {
+            //console.log(name.toLowerCase());
+
+            const matchedPlayers = playerData.filter(player => {
+                //console.log(player.playerName.toLowerCase());
+                //console.log(player.playerName.toLowerCase() === name.toLowerCase());
+                return player.playerName.toLowerCase() === name.toLowerCase();
+            })
+
+            //console.log(matchedPlayers);
+
+            // case for no matches
+            if (matchedPlayers.length === 0) {
+                await interaction.editReply(`No players found matching "${name}".`);
+                return
+            }
+
+            const rows = matchedPlayers.map((player, index) => {
                 return `**${player.playerName}** (${player.playerTeamName}): **${player.playerId}**`;
             });
 
             const embed = new EmbedBuilder()
-                .setTitle("Player Ids:")
+                .setTitle(`Player Ids for name "${name}":`)
                 .setColor("#77d32c") //green
                 .setDescription("\n" + rows.join("\n\n"))
                 .setTimestamp();
